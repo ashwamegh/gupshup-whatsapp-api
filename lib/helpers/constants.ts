d752f6408ca3c149f7d9ab6a897e70a9ef1fbd5f
Shashank Shekhar <shashank7200@gmail.com>
2021-01-17 18:10:07 +0530

    updated gupshup base uri

--- lib/helpers/constants.ts
+++ lib/helpers/constants.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-export const API_URL = "https://api.gupshup.io/sm/api/v1/msg"
+export const API_URL = "https://api.gupshup.io/sm/api/v1"
 
 export function isNonNullObject(input) {
 	return !!input && typeof input === "object" && !Array.isArray(input);
